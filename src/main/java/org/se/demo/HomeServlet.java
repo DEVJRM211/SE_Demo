9de7b5c778e9f95f55459d72551b92e2e0c8a949
devjrm211 <dev.jrm211@gmail.com>
2020-01-09 18:43:40 +0530

    commit 9Jan20 1843

---
+++ src/main/java/org/se/demo/HomeServlet.java
@@ -0,0 +1,13 @@
+package org.se.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HomeServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
